14fa0e7996bafa04c8639cda3ea11a222eb2aa21
parvathi <parvathiposa7@gmail.com>
2022-09-27 10:26:39 +0530

    updated with postgrresql

--- Emp_Test/app.py
+++ Emp_Test/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
-from service import check_user
+from dao import DataAccessObject
+from service import check_user, add_user
 
 app = Flask(__name__)
 
@@ -10,25 +11,41 @@ API Implementation
 Backend code 
 '''
 
+@app.route('/', methods=['GET'])
+def home():
+    return "<h1>App is in the Progress, Just hold Your Patience.</h1>"
+
+@app.route("/createdb", methods=['GET'])
+def create_db():
+    with DataAccessObject("localhost", "postgres", "postgres", "12345678", 5432) as cursor:
+        cursor.execute("CREATE TABLE employee(FirstName VARCHAR(100), LastName VARCHAR(100), Eid INTEGER, UserId INTEGER, Password VARCHAR(50), MobileNo VARCHAR(12), EmailId VARCHAR(50), DOB VARCHAR(50), Address VARCHAR(50), Gender VARCHAR(10), DOJ VARCHAR(50), Technology VARCHAR(50))")
+    return "<h2>Table created successfully.</h2>"
+
+
+
 @app.route('/esignup/',methods = ['POST'])
 def esignup():
     data = request.get_json()
     print("Data : ", data)
     print("Signup operation in Progress")
 
-    is_exists = check_user(data['userid'], data['eid'])
-    if is_exists:
+    is_exists = check_user(data['eid'], data['userid'])
+    if is_exists == True:
         # Server side validation
         # Pass data to service layer
-        return "Success"
+        return {"message": "The user already exist, please try again with different user."}
+
     '''
     1. Check userid, Eid exists in db or not 
         1. If exists send error message
         2. Else pass data to service layer
     '''
 
-    return "Success"
+    
+    resp = add_user(data['firstname'], data['lastname'], data['eid'], data['userid'], data['password'], data['mobileno'], data['emailid'], data['dob'], data['address'], data['gender'], data['doj'], data['technology'])
+
+    return resp
 
 
 if __name__ == '__main__':
-    app.run(host='localhost', port=4444)
+    app.run(debug = True)
--- Emp_Test/dao.py
+++ Emp_Test/dao.py
@@ -1,12 +1,104 @@
+import psycopg2
+import logging
 
+class DataAccessObject:
+    def __init__(self, host, database, user, password, port):
+        self.host = host
+        self.database = database
+        self.user = user
+        self.password = password
+        self.port = port
+        self.connection = psycopg2.connect(host=self.host,
+                database=self.database,
+                user=self.user,
+                password=self.password,
+                port=self.port)
+
+    def __enter__(self):
+        logging.info("Calling __enter__")
+        return self.connection.cursor()
+
+    def __exit__(self, error: Exception, value: object, traceback: object):
+        logging.info("Calling __exit__")
+        self.connection.commit()
+        self.connection.close()
+
+
+"""
+class DataAccessObject:
+    def __init__(self, host, database, user, password, port):
+        self.host = host
+        self.database = database
+        self.user = user
+        self.password = password
+        self.port = port
+    def establish_db_connection(self):
+        conn = ""
+        cursor = ""
+        try:
+            conn = psycopg2.connect(
+                host=self.host,
+                database=self.database,
+                user=self.user,
+                password=self.password,
+                port=self.port
+            )
+            cursor = conn.cursor()
+        except Exception as err:
+            print("Exception occurred", err)
+        return [cursor, conn]
+    def closing_connection(self):
+        cursor, conn = self.establish_db_connection()
+        cursor.close()
+        conn.close()
+    def table_creation(self):
+        cursor, conn = self.establish_db_connection()
+        query = "CREATE TABLE employee(FirstName VARCHAR(100), LastName VARCHAR(100), Eid INTEGER, UserId VARCHAR(100), Password VARCHAR(50), MobileNo VARCHAR(12), EmailId VARCHAR(50), DOB VARCHAR(50), Address VARCHAR(50), Gender VARCHAR(10), DOJ VARCHAR(50), Technology VARCHAR(50))"
+        cursor.execute(query)
+        conn.commit()
+        self.closing_connection()
+        return "Table is created"
+    def data_insertion(self):
+        cursor, conn = self.establish_db_connection()
+        records = self.data_frm_service
+        query = "INSERT INTO employee_information values(%s, %s, %s, %s)"
+        count = 0
+        for record in records:
+            cursor.execute(query, record)
+            count += 1
+            print("query executed")
+        print("Query executed", count)
+        conn.commit()
+        print("Table entries inserted")
+        self.closing_connection()
+"""
 
 
 
 def db_ucheck(a1,a2):
-    # Get db connection
-    # cursor ojbect
-    # select query
-    # if records == 0: return False
-    # else return True
-    pass
+    # Db Connection
+    with DataAccessObject("localhost", "postgres", "postgres", "12345678", 5432) as cursor:
+        # cursor ojbect
+        # select query
+        cursor.execute("SELECT * FROM employee WHERE eid={} AND userid={}".format(a1, a2))
+        data = cursor.fetchone()
+        if data == None:
+            return False
+        return True
 
+def db_insert(*args):
+    firstname, lastname, eid, userid, password, mobileno, emailid, dob, address, gender, doj, technology = args
+    record = (firstname, lastname, eid, userid, password, mobileno, emailid, dob, address, gender, doj, technology)
+    query = "INSERT INTO employee values(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+
+    with DataAccessObject("localhost", "postgres", "postgres", "12345678", 5432) as cursor:
+        # cursor.execute(f"INSERT INTO employee(firstname, lastname, eid, userid, password, mobileno, emailid, dob, address, gender, doj, technology) values({firstname}, {lastname}, {eid}, {userid}, {password}, {mobileno}, {emailid}, {dob}, {address}, {gender}, {doj}, {technology})")
+        cursor.execute(query, record)
+    
+    return {"message":f"{firstname} {lastname} user created successfully."}
+    
+
+
+if __name__ == "__main__":
+    db_ucheck(1, 2)
+    
